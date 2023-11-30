5b5ff463b60d8e69cf4b79a0f692818b4658a6b1
meena jain <meena8892jainn@gmail.com>
2023-11-30 15:25:09 +0530

    setup store file using redux and  redux thunk

---
+++ client/src/redux/store.js
@@ -0,0 +1,9 @@
+// store.js
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+  applyMiddleware(thunk)
+);
+
+export default store;
