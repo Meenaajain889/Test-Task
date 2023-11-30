92e730c6bd56d0e905a92cc48982cdf1431b5a37
meena jain <meena8892jainn@gmail.com>
2023-11-30 16:04:41 +0530

    second block update redux state while update input

--- client/src/components/secondBlock.jsx
+++ client/src/components/secondBlock.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux"
 
 const SecondBlock = () => {
+    const dispatch = useDispatch();
+    const text = useSelector((state) => state.initialText);
 
   const handleChange = (e) => {
-
+    dispatch({ type: 'UPDATE_TEXT', payload: e.target.value });
   };
 
   return (
     <div>
-      <input type="text" value={""} onChange={handleChange} />
+      <h2>{text}</h2>
+      <input type="text" value={text} onChange={handleChange} />
     </div>
   );
 };
