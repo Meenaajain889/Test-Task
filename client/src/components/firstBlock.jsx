bfcbf78156a09ed8332f2f5c639f6640215ac166
meena jain <meena8892jainn@gmail.com>
2023-11-30 15:59:38 +0530

    first block updates redux state after click button

--- client/src/components/firstBlock.jsx
+++ client/src/components/firstBlock.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from "react-redux"
 
 const FirstBlock = () => {
-
+    const [value, setValue] = useState("Hello")
     const dispatch = useDispatch();
     const text = useSelector((state) => state.initialText);
 
 
     const handleChange = (e) => {
-
+     setValue(e.target.value)
     };
+
+    const handleAddText = () => {
+        dispatch({ type: 'UPDATE_TEXT', payload: value });
+        setValue("")
+    }
 
     return (
         <div>
-            <input type="text" value={text} onChange={handleChange} />
-            <button>Add Text</button>
+            <h2>{text}</h2>
+            <input type="text" value={value} onChange={handleChange} />
+            <button onClick={handleAddText}>Add Text</button>
         </div>
     );
 };
